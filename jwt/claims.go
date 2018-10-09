9228286ff926d064329c3b427f9f1aa4ba35fd78
José Vega <jvegamoreno@outlook.com>
2018-10-09 12:34:27 +0200

    Adding some documentation

--- jwt/claims.go
+++ jwt/claims.go
@@ -1,3 +1,4 @@
+//Package jwt claims information: https://tools.ietf.org/html/rfc7519#section-4
 package jwt
 
 import (
@@ -21,107 +22,130 @@ const (
 	tokenIDk    = `jti`
 )
 
-//Issuer
+//Issuer is the entity that issued this token.
+//This is very often application specific.
+//OPTIONAL | StringOrURI | 'iss'
 func (jwt JWT) Issuer() string {
 	return jwt.Payload[issuerk].(string)
 }
 
-//SetIssuer
+//SetIssuer is the setter method for this claim.
+//Will override any existing one.
 func (jwt *JWT) SetIssuer(iss string) {
 	jwt.Payload[issuerk] = iss
 }
 
-//DelIssuer
+//DelIssuer deletes the previous issuer.
 func (jwt *JWT) DelIssuer() {
 	delete(jwt.Payload, issuerk)
 }
 
-//Subject
+//Subject identifies who is the subject of this JWT.
+//It can refers, for example, the user in your sistem that
+//this JWT is giving access to, or storing information of.
+//The subject value MUST either be scoped to be
+//locally unique in the context of the issuer or be globally unique.
+//OPTIONAL | StringOrURI | 'sub'
 func (jwt JWT) Subject() string {
 	return jwt.Payload[subjectk].(string)
 }
 
-//SetSubject
+//SetSubject setter method of this claim
+//Will override any existing one.
 func (jwt *JWT) SetSubject(sub string) {
 	jwt.Payload[subjectk] = sub
 }
 
-//DelSubject
+//DelSubject deletes the previous subject.
 func (jwt *JWT) DelSubject() {
 	delete(jwt.Payload, subjectk)
 }
 
-//Audience
+//Audience identifies the recipients that this JWT is intended for.
+//If this JWT is presented to anyone outside of the audience, the JWT
+//MUST be discarded.
+//  In the general case, the "aud" value is an array of case-
+// sensitive strings, each containing a StringOrURI value.
+// OPTIONAL | [StringOrURI] | 'aud'
 func (jwt JWT) Audience() []string {
 	return jwt.Payload[audiencek].([]string)
 }
 
-//SetAudience
+//SetAudience setter method for this claim.
+//The new aud will override the previous one.
 func (jwt *JWT) SetAudience(aud []string) {
 	jwt.Payload[audiencek] = aud
 }
 
-//DelAudience
+//DelAudience delete the previous audience list.
 func (jwt *JWT) DelAudience() {
 	delete(jwt.Payload, audiencek)
 }
 
-//ExpirationTime
+//ExpirationTime is the time in which this JWT is no longer
+//a valid one and thus, it has to be rejected.
+// OPTIONAL | seconds (numeric) | 'exp'
 func (jwt JWT) ExpirationTime() int64 {
 	return ExtractTimeField(jwt, expirationk)
 }
 
-//SetExpirationTime
+//SetExpirationTime setter method for this claim
 func (jwt *JWT) SetExpirationTime(exp int64) {
 	jwt.Payload[expirationk] = exp
 }
 
-//DelExpirationTime
+//DelExpirationTime deletes the previous expiration time
 func (jwt *JWT) DelExpirationTime() {
 	delete(jwt.Payload, expirationk)
 }
 
-//NotBefore
+//NotBefore is the time from which this JWT is valid one.
+//If this token is shown before that time, then it must be discarded.
+// OPTIONAL | seconds (numeric) | 'nbf'
 func (jwt JWT) NotBefore() int64 {
 	return ExtractTimeField(jwt, notBeforek)
 }
 
-//SetNotBefore
+//SetNotBefore setter method for this claim
 func (jwt *JWT) SetNotBefore(nbf int64) {
 	jwt.Payload[notBeforek] = nbf
 }
 
-//DelNotBefore
+//DelNotBefore deletes the previous not before timestamp.
 func (jwt *JWT) DelNotBefore() {
 	delete(jwt.Payload, notBeforek)
 }
 
-//IssuedAt
+//IssuedAt is the moment this token was issued by the issuer.
+// OPTIONAL | seconds (numeric) | 'iat'
 func (jwt JWT) IssuedAt() int64 {
 	return ExtractTimeField(jwt, issuedAtk)
 }
 
-//SetIssuedAt
+//SetIssuedAt setter method for this claim
 func (jwt *JWT) SetIssuedAt(iat int64) {
 	jwt.Payload[issuedAtk] = iat
 }
 
-//DelIssuedAt
+//DelIssuedAt deletes the previous issued at timestamp.
 func (jwt *JWT) DelIssuedAt() {
 	delete(jwt.Payload, issuedAtk)
 }
 
-//TokenID
+//TokenID is a unique identifier for the token.
+//Please, see https://tools.ietf.org/html/rfc7519#section-4.1.7
+//for a complete description of this claim.
+//OPTIONAL | StringOrURI | 'jti'
 func (jwt JWT) TokenID() string {
 	return jwt.Payload[tokenIDk].(string)
 }
 
-//SetTokenID
+//SetTokenID setter method for this claim
 func (jwt *JWT) SetTokenID(jti string) {
 	jwt.Payload[tokenIDk] = jti
 }
 
-//DelTokenID
+//DelTokenID deletes the previous token ID
 func (jwt *JWT) DelTokenID() {
 	delete(jwt.Payload, tokenIDk)
 }
